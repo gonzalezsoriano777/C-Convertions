88f06d5d2b5d6f536f9421f9d46bc8e9d4fcd194
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-04 23:26:37 +0000

    Work

---
+++ sumFiboncii.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int subFib(int num);
+
+int main(void)
+{
+  int n1 = 1;
+  int n2 = 1;
+  int sum = 2;
+  int n3 = 2;
+
+  int subFib(num)
+  {
+      for (int i = 0; i <= num; i++)
+
+  }
+
+}
