8a1950c4f121692e19f3141d53a11174e7ec7f18
Hector Gonzalez <raftb.gonzalez@outlook.com>
2018-12-04 21:40:46 +0000

    Success

--- factorlize.c
+++ factorlize.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    string value = get_string("Place name: ");
-    printf("%s\n", value);
+
+   int numb = get_int("Number: ");
+   long long v = 1;
+   if(numb == 1 || numb == 0)
+   {
+       printf("%lli\n", v);
+   }
+   else
+   {
+       for(int i = 1; i < numb; i++)
+       {
+           v = v * 1;
+       }
+       printf("%lli\n", numb * v);
+   }
 }
